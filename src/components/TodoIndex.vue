ad15bbd53232798d3658351429a1f49a4ba946a5
David <idavid@fastmail.com>
2020-04-06 10:10:59 -0600

    per page

--- src/components/TodoIndex.vue
+++ src/components/TodoIndex.vue
@@ -3,18 +3,15 @@
 </template>
 
 <script>
-// import { ref } from 'vue'
-
 export default {
   async setup() {
-    const todos = await fetch('https://jsonplaceholder.typicode.com/todos')
+    const todos = await fetch('https://jsonplaceholder.typicode.com/todos?_page=1&_limit=5')
       .then(response => response.json())
 
     return {
         todos
     }
   }
-
 }
 </script>
 
