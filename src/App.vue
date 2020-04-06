df4cdcfc842e0f535c2cf44f9c612c07915d628c
David <idavid@fastmail.com>
2020-04-06 09:19:42 -0600

    wip new features Vue3

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,25 @@
 <template>
-  <img src="./logo.png">
   <h1>Hello Vue 3!</h1>
+  <Suspense>
+    <template #default>
+      <todo-index />
+    </template>
+    <template #fallback>
+      <div>Loading...</div>
+    </template>
+  </Suspense>
+  <hr>
   <button @click="inc">Clicked {{ count }} times.</button>
 </template>
 
 <script>
 import { ref } from 'vue'
+import TodoIndex from './components/TodoIndex.vue'
 
 export default {
+  components: {
+    TodoIndex
+  },
   setup() {
     const count = ref(0)
     const inc = () => {
---
+++ src/components/TodoIndex.vue
@@ -0,0 +1,22 @@
+<template>
+    {{  todos }}
+</template>
+
+<script>
+// import { ref } from 'vue'
+
+export default {
+  async setup() {
+    const todos = await fetch('https://jsonplaceholder.typicode.com/todos')
+      .then(response => response.json())
+
+    return {
+        todos
+    }
+  }
+
+}
+</script>
+
+<style lang="css" scoped>
+</style>
