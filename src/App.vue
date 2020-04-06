2ba2169007921722877442242e2cc78194da15f8
David <idavid@fastmail.com>
2020-04-06 10:11:20 -0600

    onError add error

--- src/App.vue
+++ src/App.vue
@@ -2,29 +2,42 @@
   <h1>Hello Vue 3!</h1>
   <Suspense>
     <template #default>
+      <h3>Todos</h3>
       <todo-index />
+      <hr>
+      <h3>Posts</h3>
+      <posts />
+      <hr>
+      <h3>Users</h3>
+      <users />
     </template>
     <template #fallback>
       <div>Loading...</div>
     </template>
   </Suspense>
   <hr>
-  <button @click="inc">Clicked {{ count }} times.</button>
+  <!-- <button @click="inc">Clicked {{ count }} times.</button> -->
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref, onErrorCaptured } from 'vue'
 import TodoIndex from './components/TodoIndex.vue'
+import Posts from './components/Posts.vue'
+import Users from './components/Users.vue'
 
 export default {
   components: {
-    TodoIndex
+    TodoIndex,
+    Posts,
+    Users
   },
   setup() {
     const count = ref(0)
     const inc = () => {
       count.value++
     }
+
+    onErrorCaptured(e => console.error('error ==> ',e))
 
     return {
       count,
