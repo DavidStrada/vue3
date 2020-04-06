b01ba1588760388f9d3091219d26f5cfd35fcfdb
David <idavid@fastmail.com>
2020-04-06 10:10:51 -0600

    produces an error to test  onErrorCaptured

---
+++ src/components/Users.vue
@@ -0,0 +1,18 @@
+<template>
+    {{ users }}
+</template>
+
+<script>
+export default {
+  async setup() {
+    const users = await Promise.reject(new Error('fail'));
+
+    return {
+        users
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
