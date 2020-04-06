680332f4d445888d824ec8275ca6ba1e784ec57f
David <idavid@fastmail.com>
2020-04-06 10:11:06 -0600

    fetches posts

---
+++ src/components/Posts.vue
@@ -0,0 +1,19 @@
+<template>
+    {{ posts }}
+</template>
+
+<script>
+export default {
+  async setup() {
+    const posts = await fetch('https://jsonplaceholder.typicode.com/posts?_page=1&_limit=5')
+      .then(response => response.json())
+
+    return {
+        posts
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
